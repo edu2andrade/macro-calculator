69f5c6b887a11272470a32e1a018cc8ccd407c07
edu2andrade <edu2andrade>
2021-12-27 16:19:26 +0100

    Harris, Mifflin and average formulas added

--- script.js
+++ script.js
@@ -4,16 +4,27 @@ const peso = document.getElementById("peso");
 const altura = document.getElementById("altura");
 const edad = document.getElementById("edad");
 
-const BodyMassRatio = () => {
-  sexoMasc.checked
-    ? 66.5 + 13.75 * peso.value + 5.003 * altura.value - 6.775 * edad.value
-    : 665.1 + 9.563 * peso.value + 1.85 * altura.value - 4.676 * edad.value;
-  // Verifica o sexo e imprime o BMR na tela (testa com console.log primeiro) dá undefined...
-};
-
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  BodyMassRatio();
-  console.log("testes --> ", sexoMasc.checked);
-  console.log(BodyMassRatio());
+  console.log("Harris -->", HarrisBenedict());
+  console.log("Mifflin -->", Mifflin());
+  console.log("average -->", (HarrisBenedict() + Mifflin())/2)
 });
+
+const HarrisBenedict = () => {
+  // Check genre and returns the BMR accordingly
+  const result = sexoMasc.checked
+  ? 66.5 + 13.75 * peso.value.trim() + 5.003 * altura.value.trim() - 6.775 * edad.value.trim()
+  : 665.1 + 9.563 * peso.value.trim() + 1.85 * altura.value.trim() - 4.676 * edad.value.trim();
+
+  return result;
+};
+
+const  Mifflin = () => {
+  // Check genre and returns the BMR accordingly (different formula)
+  const result = sexoMasc.checked
+  ? 5 + (9.99 * peso.value.trim()) + (6.25 * altura.value.trim()) - (4.92 * edad.value.trim())
+  : -161 + (9.99 * peso.value.trim()) + (6.25 * altura.value.trim()) - (4.92 * edad.value.trim());
+
+  return result;
+}
