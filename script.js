3944b9ab0db856c599740f2c5aa9e0fe2fbb54b1
edu2andrade <edu2andrade>
2021-12-29 09:48:21 +0100

    translate all code to english, harrisBenedict and mifflin updated, selectedTd selecetedTdee, basicRule, caloricDailyNeeds implemented

--- script.js
+++ script.js
@@ -1,30 +1,158 @@
 const form = document.getElementById("calc");
-const sexoMasc = document.getElementById("sexo-masc");
-const peso = document.getElementById("peso");
-const altura = document.getElementById("altura");
-const edad = document.getElementById("edad");
+const genreMale = document.getElementById("genreMale");
+const weight = document.getElementById("weight");
+const height = document.getElementById("height");
+const age = document.getElementById("age");
+const tdee = document.querySelectorAll("input[name='tdee']");
+const goal = document.getElementById("goal");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  console.log("Harris -->", HarrisBenedict());
-  console.log("Mifflin -->", Mifflin());
-  console.log("average -->", (HarrisBenedict() + Mifflin())/2)
+  checkMacros();
 });
 
-const HarrisBenedict = () => {
+// Function tdee's selector:
+function selectedTdee() {
+  let result;
+  for (let i of tdee) {
+    i.checked
+    ? result = i.value
+    : null;
+  }
+  return result;
+}
+
+// Function tdee's calcs: (NOT WORKING... WHY???)
+/* function tdeeCalc(selectedValue, bmr) {
+  switch (selectedValue) {
+    case "sedentary":
+      result = bmr * 1.3;
+      break;
+    case "active":
+      result = bmr * 1.475;
+      break;
+    case "very-active":
+      result = bmr * 1.8;
+      break;
+    case "extrem-active":
+      result = bmr * 2.1;
+      break;
+    default: 
+      result = 0;
+    }
+  
+  return result;
+} */
+
+const harrisBenedict = () => {
   // Check genre and returns the BMR accordingly
-  const result = sexoMasc.checked
-  ? 66.5 + 13.75 * peso.value.trim() + 5.003 * altura.value.trim() - 6.775 * edad.value.trim()
-  : 665.1 + 9.563 * peso.value.trim() + 1.85 * altura.value.trim() - 4.676 * edad.value.trim();
+  // BMR:
+  const bmr = genreMale.checked
+  ? 66.5 + 13.75 * weight.value.trim() + 5.003 * height.value.trim() - 6.775 * age.value.trim()
+  : 665.1 + 9.563 * weight.value.trim() + 1.85 * height.value.trim() - 4.676 * age.value.trim();
+
+  // calcs accordingly genre and tdee:
+  // tdeeCalc(bmr, selectedValue); (NOT WORKING...)
+  switch (selectedTdee()) {
+  case "sedentary":
+    result = bmr * 1.3;
+    break;
+  case "active":
+    result = bmr * 1.475;
+    break;
+  case "very-active":
+    result = bmr * 1.8;
+    break;
+  case "extrem-active":
+    result = bmr * 2.1;
+    break;
+  default: 
+    result = 0;
+  }
 
   return result;
 };
 
-const  Mifflin = () => {
+const mifflin = () => {
   // Check genre and returns the BMR accordingly (different formula)
-  const result = sexoMasc.checked
-  ? 5 + (9.99 * peso.value.trim()) + (6.25 * altura.value.trim()) - (4.92 * edad.value.trim())
-  : -161 + (9.99 * peso.value.trim()) + (6.25 * altura.value.trim()) - (4.92 * edad.value.trim());
+  const bmr = genreMale.checked
+  ? 5 + (9.99 * weight.value.trim()) + (6.25 * height.value.trim()) - (4.92 * age.value.trim())
+  : -161 + (9.99 * weight.value.trim()) + (6.25 * height.value.trim()) - (4.92 * age.value.trim());
 
+  // calcs accordingly genre and tdee: (CODE REPETITION HERE!)
+  switch (selectedTdee()) {
+    case "sedentary":
+      result = bmr * 1.3;
+      break;
+    case "active":
+      result = bmr * 1.475;
+      break;
+    case "very-active":
+      result = bmr * 1.8;
+      break;
+    case "extrem-active":
+      result = bmr * 2.1;
+      break;
+    default: 
+      result = 0;
+    }
+  
+    return result;
+};
+
+const basicRule = () => {
+  // For TDEE calcs: (the calcs here are different...)
+  switch (selectedTdee()) {
+    case "sedentary":
+      result = 28.5 * (weight.value / 2.2);
+      break;
+    case "active":
+      result = 33.5 * (weight.value / 2.2);
+      break;
+    case "very-active":
+      result = 39.5 * (weight.value / 2.2);
+      break;
+    case "extrem-active":
+      result = 0;
+      break;
+    default: 
+      result = 0;
+  }
   return result;
+}
+
+// Caloric daily needs according goal (DONE!!)
+const caloricDailyNeeds = () => {
+
+  const adjustedAverageTdee = 
+  selectedTdee() === "extrem-active"
+  ? (harrisBenedict() + mifflin()) / 2
+  : (harrisBenedict() + mifflin() + basicRule())/3;
+
+  console.log("goal -->", goal.value);
+  console.log("adjustedTdee -->", adjustedAverageTdee);
+
+  switch (goal.value) {
+    case "gain-weight":
+      result = adjustedAverageTdee + (adjustedAverageTdee * 0.1);
+      break;
+    case "keep-the-same":
+      result = adjustedAverageTdee;
+      break;
+    case "loose-weight":
+      result = adjustedAverageTdee - (adjustedAverageTdee * 0.1);
+      break;
+    default:
+      result = "Please select a goal first!!!";
+  }
+  return result;
+}
+
+// Macro calculations:
+
+function checkMacros() {
+  console.log("Harris -->", harrisBenedict());
+  console.log("Mifflin -->", mifflin());
+  console.log("basicRule -->", basicRule());
+  console.log("caloricDailyNeeds -->", caloricDailyNeeds());
 }
